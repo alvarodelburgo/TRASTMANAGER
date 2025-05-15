ba229b3e91bd3b9c20a67720603d3a7f7825391f
alvaro <alvarodbperez@gmail.com>
2025-05-15 12:12:00 +0200

    Subiendo archivos actualizados

--- iac/main.tf
+++ iac/main.tf
@@ -87,7 +87,7 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = var.my_ip
+    cidr_blocks = [var.my_ip]
   }
 
   # Salida para todo el tráfico
